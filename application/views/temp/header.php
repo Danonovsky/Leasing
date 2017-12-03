f57ed427346a69e67363f3371261547582eefdb7
Danonovsky <daniel.jozefiuk@gmail.com>
2017-12-03 14:36:34 +0100

    Fix

--- application/views/temp/header.php
+++ application/views/temp/header.php
@@ -4,8 +4,10 @@
   <meta charset="utf-8">
   <title>LeasIt - <?=$title?></title>
 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?=base_url('assets\css\bootstrap.min.css')?>">
   <link rel="stylesheet" href="<?=base_url('assets\css\style.css')?>">
+
 
 </head>
 <body>
